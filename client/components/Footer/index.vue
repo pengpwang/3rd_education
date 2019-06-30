cad865e14f97f399b42fcfd46c4dfcebf0337173
wangpengpeng <wangpengpeng@yit.com>
2019-06-30 20:13:33 +0800

    add

--- client/components/Footer/index.vue
+++ client/components/Footer/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer class="Footer"></footer>
+  <footer class="Footer">
+    <div class="footer-coryright">版权所有：慧考拉</div>
+  </footer>
 </template>
 
 <script>
@@ -10,8 +12,20 @@ export default {
 
 <style scoped>
 .Footer{
+  width: 100%;
   height: 392px;
   background: url(~assets/img/footer.png);
+  position: relative;
+}
+
+.footer-coryright{
+  width: 1200px;
+  position: absolute;
+  left: 50%;
+  top: 68px;
+  margin-left: -600px;
+  color:#fff;
+  font-size: 16px;
 }
 </style>
 
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -33,10 +33,173 @@
         </div>
       </div>
     </div>
+    <div class="index-consultation">
+      <div class="index-article-w">
+        <div class="index-article-tag">考试资讯</div>
+        <div class="index-article-list">
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+          <a class="index-article-list-item" href="">欢迎来到慧考拉官网欢迎来到慧考拉官网</a>
+        </div>
+      </div>
+      <div class="index-pic-w">
+        <div class="index-pic">
+          <div class="index-pic-item">
+            <img class="index-pic-img" src="~assets/img/index/consultation-pic.png" alt=""/>
+            <div class="index-pic-banner">欢迎来到慧考拉官网</div>
+          </div>
+          <div class="index-pic-item">
+            <img class="index-pic-img" src="~assets/img/index/consultation-pic.png" alt=""/>
+            <div class="index-pic-banner">欢迎来到慧考拉官网</div>
+          </div>
+        </div>
+        <div class="index-pic">
+          <div class="index-pic-item">
+            <img class="index-pic-img" src="~assets/img/index/consultation-pic.png" alt=""/>
+            <div class="index-pic-banner">欢迎来到慧考拉官网</div>
+          </div>
+          <div class="index-pic-item">
+            <img class="index-pic-img" src="~assets/img/index/consultation-pic.png" alt=""/>
+            <div class="index-pic-banner">欢迎来到慧考拉官网</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="index-data-download">
+      <div class="index-data-download-title">资料下载</div>
+      <div class="index-data-download-w">
+        <div class="index-data-download-pic bg-r">
+          <div class="index-data-download-pic-title">成人学历考试历年模拟题题库</div>
+          <div class="index-data-download-pic-ls">
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+          </div>
+        </div>
+        <div class="index-data-download-pic bg-g">
+          <div class="index-data-download-pic-title">成人学历考试历年模拟题题库</div>
+          <div class="index-data-download-pic-ls">
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+          </div>
+        </div>
+        <div class="index-data-download-pic bg-b">
+          <div class="index-data-download-pic-title">成人学历考试历年模拟题题库</div>
+          <div class="index-data-download-pic-ls">
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+            <div class="zliao_download">
+              <img class="zliao_download_icon" src="~assets/img/index/data_down_icon.png" alt=""/>
+              <a class="zliao_download_a" href="">2018年成人学历考试历年模拟题</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="index-promote">
+      <div class="index-promote-w">
+        <div class="index-promote-title">学历提升</div>
+        <div class="index-promote-w-c">
+          <img class="index-promote-banner" src="~assets/img/index/promote.png" alt=""/>
+          <div class="index-promote-article">全阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门全面阐述了出门</div>
+        </div>
+      </div>
+    </div>
+    <div class="index-promote">
+      <div class="index-promote-w">
+        <div class="index-promote-title">师资风采</div>
+        <div class="index-promote-w-c teacher-fen">
+          <el-carousel trigger="click" height="400px">
+            <el-carousel-item v-for="item in 3" :key="item">
+              <div class="teacher-introduce">
+                <img class="teacher-photo" src="~assets/img/index/teacher_1.png" alt=""/>
+                <div class="teacher-txt">
+                  <div class="teacher-name">老师姓名</div>
+                  <div class="teacher-jieshao">老师的介绍老师的介绍老师的介绍老师的介绍老师的介绍</div>
+                </div>
+              </div>
+              <div class="teacher-introduce teacher-center">
+                <img class="teacher-photo" src="~assets/img/index/teacher_2.png" alt=""/>
+                <div class="teacher-txt">
+                  <div class="teacher-name">老师姓名</div>
+                  <div class="teacher-jieshao">老师的介绍老师的介绍老师的介绍老师的介绍老师的介绍</div>
+                </div>
+              </div>
+              <div class="teacher-introduce">
+                <img class="teacher-photo" src="~assets/img/index/teacher_3.png" alt=""/>
+                <div class="teacher-txt">
+                  <div class="teacher-name">老师姓名</div>
+                  <div class="teacher-jieshao">老师的介绍老师的介绍老师的介绍老师的介绍老师的介绍</div>
+                </div>
+              </div>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </div>
+    </div>
+    <div class="index-promote bg-gray">
+      <div class="index-promote-w">
+        <div class="index-promote-title">教学环境</div>
+        <div class="index-promote-w-c">
+          <div class="index-study-w">
+            <img class="index-study-w-pic" src="~assets/img/index/study_1.png" alt=""/>
+            <img class="index-study-w-pic" src="~assets/img/index/study_2.png" alt=""/>
+            <img class="index-study-w-pic" src="~assets/img/index/study_3.png" alt=""/>
+            <img class="index-study-w-pic" src="~assets/img/index/study_4.png" alt=""/>
+          </div>
+          <div class="index-study-button">查看更多</div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import { Carousel, CarouselItem } from 'element-ui'
 import Banner from '~/components/Banner'
 
 export default {
@@ -85,6 +248,347 @@ export default {
 
 .index-business-introduce-i_txt{
   margin-top: 20px;
+}
+
+.index-consultation {
+  width: 1200px;
+  height: 460px;
+  position: relative;
+  left: 50%;
+  margin-left: -600px;
+  background: rgb(246, 249, 250);
+}
+
+.index-article-w{
+  padding-top: 38px;
+  float: left;
+}
+
+.index-article-tag{
+  width: 147px;
+  height: 46px;
+  background: rgb(134, 138, 230);
+  text-align: center;
+  line-height: 46px;
+  color: #fff;
+  font-size: 20px;
+  border-top-right-radius: 23px;
+  border-bottom-right-radius: 23px;
+}
+
+.index-article-list{
+  padding-top: 50px;
+  padding-left: 22px;
+  width: 600px;
+  height: 260px;
+  overflow: hidden;
+}
+
+.index-pic-w{
+  float: right;
+  margin-top: 56px;
+}
+
+.index-article-list-item{
+  display: inline-block;
+  text-decoration: none;
+  font-size: 14px;
+  color:rgb(98, 101, 102);
+  width: 280px;
+  margin-bottom: 16px;
+  position: relative;
+  padding-left: 34px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+
+.index-article-list-item::before{
+  content: '';
+  display: block;
+  width: 21px;
+  height: 23px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  background: url(~assets/img/index/zixun_icon.png) no-repeat center center;
+  background-size: 18px 17px;
+}
+
+.index-pic-item{
+  width: 250px;
+  height: 167px;
+  margin-right: 14px;
+  margin-bottom: 14px;
+  display: inline-block;
+  position: relative;
+}
+
+.index-pic-img{
+  width: 250px;
+  height: 167px;
+}
+
+.index-pic-banner{
+  position: absolute;
+  z-index: 1;
+  width: 238px;
+  height: 34px;
+  left: 7px;
+  bottom: 5px;
+  padding-left: 14px;
+  padding-right: 14px;
+  line-height: 34px;
+  color: #fff;
+  background: rgba(132, 79, 148, 0.6);
+  font-size: 14px;
+}
+
+.index-data-download{
+  height: 785px;
+  width: 100%;
+  background: url(~assets/img/index/data-down-banner.png) center center no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.index-data-download-title{
+  position: absolute;
+  left: 50%;
+  top: 70px;
+  transform: translateX(-50%);
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -ms-transform: translate(-50%);
+  font-size: 28px;
+  color: #fff;
+}
+
+.index-data-download-title::after{
+  content: '';
+  display: block;
+  position: absolute;
+  width: 100px;
+  height: 2px;
+  background: #fff;
+  left: 50%;
+  margin-left: -50px;
+}
+
+.index-data-download-w{
+  width: 1200px;
+  position: absolute;
+  left: 50%;
+  top: 200px;
+  transform: translateX(-50%);
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -ms-transform: translate(-50%);
+}
+
+.index-data-download-pic{
+  position: relative;
+  display: inline-block;
+}
+
+.bg-g{
+  margin-left: 50px;
+  margin-right: 50px;
+}
+
+.index-data-download-pic-title{
+  position: absolute;
+  width: 294px;
+  height: 55px;
+  border: 3px solid rgb(194, 141, 241);;
+  border-radius: 27px;
+  left: 50%;
+  transform: translateX(-50%);
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -ms-transform: translate(-50%);
+  top: -27px;
+  background: #fff;
+  text-align: center;
+  line-height: 49px;
+
+}
+
+.bg-r .index-data-download-pic-title{
+  border-color: rgb(194, 141, 241);
+}
+
+.bg-r .index-data-download-pic-ls{
+  background: rgb(194, 141, 241);
+}
+
+.bg-g .index-data-download-pic-title{
+  border-color: rgb(141, 188, 73);
+}
+
+.bg-g .index-data-download-pic-ls{
+  background: rgb(141, 188, 73);
+}
+
+.bg-b .index-data-download-pic-title{
+  border-color: rgb(86, 161, 221);
+}
+
+.bg-b .index-data-download-pic-ls{
+  background: rgb(86, 161, 221);
+}
+
+.index-data-download-pic-ls{
+  width: 363px;
+  height: 420px;
+  border-radius: 6px;
+  padding: 50px;
+  overflow: hidden;
+}
+
+.zliao_download{
+  vertical-align: middle;
+  margin-bottom: 15px;
+}
+
+.zliao_download_a{
+  text-decoration: none;
+  display: inline-block;
+  vertical-align: top;
+}
+
+.index-promote{
+  height: 704px;
+  width: 100%;
+  position: relative;
+}
+
+.index-promote-w{
+  height: 704px;
+  width: 1200px;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -ms-transform: translate(-50%);
+}
+
+.index-promote-title{
+  position: absolute;
+  left: 50%;
+  top: 70px;
+  transform: translateX(-50%);
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -ms-transform: translate(-50%);
+  font-size: 28px;
+  color: #000;
+}
+
+.index-promote-title::after{
+  content: '';
+  display: block;
+  position: absolute;
+  width: 100px;
+  height: 2px;
+  background: #000;
+  left: 50%;
+  margin-left: -50px;
+}
+
+.index-promote-w-c{
+  position: absolute;
+  top: 150px;
+}
+
+.index-promote-banner{
+  float: left;
+  width: 838px;
+  height: 554px;
+}
+
+.index-promote-article{
+  text-indent:2em;
+  width:328px;
+  height: 450px;
+  float: left;
+  margin-left: 33px;
+  margin-top: 56px;
+  text-indent: 2;
+  border: 2px solid rgb(109, 32, 128);
+  border-left: none;
+  padding-top: 30px;
+  padding-right: 30px;
+  padding-bottom: 30px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.bg-gray{
+  background: rgb(246, 247, 251);
+  height: 550px;
+}
+
+.index-study-w-pic{
+  width: 293px;
+  height: 219px;
+}
+
+.index-study-button{
+  margin-top: 32px;
+  width: 118px;
+  height: 40px;
+  border: 1px solid rgb(134, 138, 230);
+  border-radius: 20px;
+  text-align: center;
+  line-height: 38px;
+  color: rgb(134, 138, 230);
+  cursor: pointer;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  -webkit-transform: translateX(-50%);
+  -moz-transform: translateX(-50%);
+  -ms-transform: translate(-50%);
+}
+
+.banner-pic{
+  width: 100%;
+  height: 400px;
+}
+
+.teacher-fen{
+  width: 100%;
+  height: 400px;
+}
+
+.teacher-introduce{
+  float: left;
+  width: 372px;
+  height: 396px;
+  box-shadow: 2px 2px 3px 3px rgb(139, 140, 141);
+}
+
+.teacher-center{
+  margin-left: 40px;
+  margin-right: 40px;
+}
+
+.teacher-txt{
+  height: 128px;
+  padding: 20px 16px;
+}
+
+.teacher-name{
+  font-size: 20px;
+  text-align: center;
+  color: rgb(82, 83, 84);
+}
+
+.teacher-jieshao{
+  text-indent: 2em;
+  color: rgb(139, 140, 141);
 }
 
 </style>
