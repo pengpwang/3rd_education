a137462d7e291e07bad876e5bf379059110ba786
wangpengpeng <wangpengpeng@yit.com>
2019-07-09 00:33:52 +0800

    add

--- client/pages/courseIntroduce.vue
+++ client/pages/courseIntroduce.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="courseIntroduce">courseIntroduce</div>
+  <div class="courseIntroduce">
+    <div class="img-wrap">
+      <img class="courseIntroduce_banner" src="~assets/img/course/course_banner.png" alt=""/>
+    </div>
+
+  </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    };
+  }
 }
 </script>
 
 
 <style scoped>
-
+.courseIntroduce{
+  position: relative;
+}
+.img-wrap{
+  width: 100%;
+  height: 586px;
+}
+.courseIntroduce_banner{
+  width: 100%;
+  height: 586px;
+}
 </style>
 
 
